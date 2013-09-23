3b6dd20ff3c99ffd2ba5796f47cb180cbe152235
Roee Ebenstein <ebenstein.2@osu.edu>
2013-09-23 00:06:08 -0400

    Basic content reader built. Current version - we get for each xml, for each article all of its content... now I'm ready to write the vector...

--- 5243hw2/src/AppLoader.java
+++ 5243hw2/src/AppLoader.java
@@ -1,4 +1,76 @@
+import java.io.File;
+import java.io.FilenameFilter;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
-public class AppLoader {
 
+public class AppLoader 
+{
+	static public void main(String[] params)
+	{
+		String XmlsPath;
+		
+		if (params.length == 0)
+			XmlsPath = "../5243hw2Py/hw1/xmls/";
+		else
+			XmlsPath = params[0];
+		
+		ReadXmlAndBuildVector(XmlsPath);
+	}
+	
+	static public void ReadXmlAndBuildVector(String XmlsPath)
+	{
+		   
+		  File dir = new File(XmlsPath);
+		  File [] files = dir.listFiles(new FilenameFilter() {
+		      @Override
+		      public boolean accept(File dir, String name) {
+		          return name.endsWith(".xml");
+		      }
+		  });
+
+		  // Getting the articles...
+		  if (files != null)
+		  {
+			  for (File xmlfile : files) 
+			  {
+			      Document doc = XmlReader.ReturnXml(xmlfile.getPath());
+			      Node[] articles = XmlReader.getElements(doc, "REUTERS");
+			      
+			      if (articles != null)
+			      {
+				      for (Node article : articles)
+				      {
+				    	  if (article.getNodeType() == Node.ELEMENT_NODE) 
+				    	  {  
+				  			Element eArticle = (Element) article;
+				  			
+				  			// For each article - extract the topics, locations, and text
+				  			int id = Integer.parseInt(eArticle.getAttribute("NEWID"));
+				  			
+							NodeList nTopics = ((Element)eArticle.getElementsByTagName("TOPICS").item(0)).getElementsByTagName("D");
+							NodeList nLocations = ((Element)eArticle.getElementsByTagName("PLACES").item(0)).getElementsByTagName("D");
+							
+							// Get Text and subjects
+							Node nText = eArticle.getElementsByTagName("TEXT").item(0);
+							NodeList nTitle = ((Element)nText).getElementsByTagName("TITLE");
+							NodeList nDateLine = ((Element)nText).getElementsByTagName("DATELINE");
+							NodeList nBody = ((Element)nText).getElementsByTagName("BODY");
+							
+							// Extract Texts
+							String[] Topics = XmlReader.getElementContent(nTopics);
+							String[] Locations = XmlReader.getElementContent(nLocations);
+							String[] Title = XmlReader.getElementContent(nTitle);
+							String[] DateLine = XmlReader.getElementContent(nDateLine);
+							String[] Body = XmlReader.getElementContent(nBody);
+							
+				  			System.out.println("Processed Document " + id);
+				    	  }
+				      }
+			      }
+			  }
+		  }
+	}
 }
---
+++ 5243hw2/src/XmlReader.java
@@ -0,0 +1,80 @@
+import javax.management.remote.TargetedNotification;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+
+
+public class XmlReader 
+{
+	static public Document ReturnXml(String FilePath)
+	{
+		Document retVal = null;
+		try {
+			 
+			File fXmlFile = new File(FilePath);
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(fXmlFile);
+			retVal = doc;
+		 
+
+			} 
+			catch (Exception e) 
+			{
+		    	e.printStackTrace();
+		    	
+		    }
+		return retVal;
+	}
+	
+	static public String[] getElementContent(Document doc,String TagNameToGet)
+	{
+		String[] retVal;
+		
+		NodeList listToScan = doc.getElementsByTagName(TagNameToGet);
+		retVal = getElementContent(listToScan);
+		
+		return retVal;
+	}
+	
+	static public String[] getElementContent(NodeList nd)
+	{
+		String[] retVal;
+
+		if (nd.getLength() > 0)
+		{
+			retVal = new String[nd.getLength()];
+		}
+		else retVal = null;
+		
+		for (int i = 0; i < nd.getLength(); i++) {
+			 
+			Node nNode = nd.item(i);
+	 
+			retVal[i] = nNode.getTextContent();
+		}
+		
+		return retVal;
+	}
+	
+	static public Node[] getElements(Document doc, String TagNameToGet)
+	{
+		Node[] retVal;
+		
+		NodeList listToScan = doc.getElementsByTagName(TagNameToGet);
+		retVal = new Element[listToScan.getLength()];
+		
+		for (int i = 0; i < listToScan.getLength(); i++) {
+			 
+			Node nNode = listToScan.item(i);
+	 
+			retVal[i] = nNode;
+		}
+		
+		return retVal;
+	}
+}
